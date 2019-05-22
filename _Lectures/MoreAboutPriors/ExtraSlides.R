104de9a3ca0c9e3820d7926e9ad94e8a792d0b94
CCheCastaldo <cccweb@icloud.com>
2019-05-22 13:00:50 -0700

    adds more about priors lecture

---
+++ _Lectures/MoreAboutPriors/ExtraSlides.R
@@ -0,0 +1,19 @@
+## Bayes Theorem 
+
+$$\underbrace{[\theta|y]}_\text{Posterior} = \frac{\overbrace{[y|\theta]}^\text{Likelihood}\overbrace{[\theta]}^\text{Prior}}{\underbrace{\int [y | \theta][\theta] d\theta}_\text{Marginal}}$$
+
+- Likelihood: Links unobserved $\theta$ to observed $y$.
+- Prior Distribution: What is already known about $\theta$.
+- Marginal Distribution: the area under the joint distribution curve. Serves to normalize the curve with respect to $\theta$.
+- Posterior Distribution: a true PDF.
+
+## Mary's Notes That Need Translated
+
+Insert meta analysis example
+
+- I went to the literature and I found things that I might consider using as a prior.
+- Show how I would use what I found to make and informed prior
+- Insert SESYNC board notes
+- Go to Meta Analysis lab and make a mini version that is not in the context of pediatric death
+
+
