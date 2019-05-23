1044ce6a9a5c7b280caa419970733980570ac241
CCheCastaldo <cccweb@icloud.com>
2019-05-23 13:11:03 -0700

    adds remaining probability lectures and labs

---
+++ _Lectures/RulesProbability/ExtraSlides.R
@@ -0,0 +1,25 @@
+## Factoring with DAGs at the board
+
+## Factoring with DAGs
+
+\centerline{\includegraphics[height=.8in]{../../_Graphics/factoringI}}
+
+$\Pr(A,B) =$ -->
+
+## Factoring with DAGs
+
+\centerline{\includegraphics[height=.8in]{../../_Graphics/factoringI}}
+
+$\Pr(A,B) = \Pr(A|B) \Pr(B)$
+
+## Factoring with DAGs
+
+\centerline{\includegraphics[height=.8in]{../../_Graphics/abcDag}}
+
+$\Pr(A,B,C) =$
+
+## Factoring with DAGs
+
+\centerline{\includegraphics[height=.8in]{../../_Graphics/abcDag}}
+
+$\Pr(A,B,C) = \Pr(A|B,C)\Pr(B|C)\Pr(C)$
