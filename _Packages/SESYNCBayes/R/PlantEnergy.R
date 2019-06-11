2a805183789b539390adf43ed460c74973693b2c
CCheCastaldo <cccweb@icloud.com>
2019-06-11 00:35:45 -0400

    adds plant energy rda to R package

---
+++ _Packages/SESYNCBayes/R/PlantEnergy.R
@@ -0,0 +1,10 @@
+#' @title Plant Energy Data
+#'
+#' @description Concentration of metabolizable energy (ME) in plants.
+#' @name PlantEnergy
+#' @docType data
+#' @format{ A data frame with 531 observations on the following 1 variables.
+#' \describe{
+#' \item{\code{ME}}{concentration of metabolizable energy}
+#'}}
+NULL
